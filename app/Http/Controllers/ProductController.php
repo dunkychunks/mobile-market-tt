3196e310e9c532550cf8253a2f418ad4a65e62b3
dunkychunks <joelcduncan@gmail.com>
2026-01-28 20:43:34 -0400

    Implemented store and details functionality
    Added code to ProductController and DetailController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Traits\PhpFlasher;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
@@ -11,9 +13,21 @@ class ProductController extends Controller
 
     /**
      * Display a listing of the resource.
+     * This function pulls products from the Products table
+     * The information is then sent to the products page to be displayed
      */
     public function index()
     {
-        return true;
+        //Check to see if user is logged in and which groups they belong to e.g. Amazon prime member discounts
+
+        $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
+
+        //retrieves products from the Products table
+
+        $product_data = Product::withPrices()->get();
+
+        //pass data to the Products page to display
+
+        return view('pages.default.productspage', compact('product_data'));
     }
 }
